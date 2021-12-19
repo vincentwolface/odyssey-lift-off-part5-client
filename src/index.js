042578a02eca99fb7936e80fd5db86949b53f597
VincentHilti <visdvin@hilti.com>
2021-12-19 16:45:10 +0100

    Change server Url to match Heroku server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://server-castronauts-viny.herokuapp.com/', // 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
